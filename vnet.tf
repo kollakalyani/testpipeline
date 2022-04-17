89a9b3c42b9f5235f8116966ee9c534c113d02cb
kollakalyani <100208567+kollakalyani@users.noreply.github.com>
2022-04-18 00:34:52 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "piprg"
+  name     = "piprg1"
   location = "east asia"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "vnetpipe"
+  name                = "vnetpip01"
   location            = "east asia"
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["14.0.0.0/16"]
+  address_space       = ["11.0.0.0/16"]
 
   subnet {
     name           = "subnet1"
-    address_prefix = "14.0.1.0/24"
+    address_prefix = "11.0.1.0/24"
   }
 
   tags = {
